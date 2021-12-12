8f4ebcd3a8e679858a8f5857cd59c7e7248dcca6
kupaqu <39209065+kupaqu@users.noreply.github.com>
2021-12-12 19:48:48 +0800

    Delete target directory

--- target/debug/deps/math_expression_evaluator-7851fe21a59e85da.d
+++
@@ -1,6 +0,0 @@
-C:\Users\Buda\Documents\progLangs\math-expression-evaluator\target\debug\deps\math_expression_evaluator-7851fe21a59e85da.exe: src\main.rs src\token.rs
-
-C:\Users\Buda\Documents\progLangs\math-expression-evaluator\target\debug\deps\math_expression_evaluator-7851fe21a59e85da.d: src\main.rs src\token.rs
-
-src\main.rs:
-src\token.rs:
